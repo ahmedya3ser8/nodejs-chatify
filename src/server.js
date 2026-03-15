84ec15ae170271e1dc13099132379e0275bb7c94
ahmed yasser <ahmedya3ser8@gmail.com>
2026-03-16 01:51:34 +0200

    feat: add signup controller, model, routes

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
+
+app.use(express.json()); // req.body
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running on port 3000');
+  console.log(`Server running on port ${process.env.PORT}`);
+  connectDB();
 })
