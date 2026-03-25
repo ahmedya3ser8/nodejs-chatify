883ffa38a0c77861d1a17632f568c64ea8c20167
ahmed yasser <ahmedya3ser8@gmail.com>
2026-03-25 17:42:52 +0200

    feat: fix cookies

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,8 +9,8 @@ export const generateToken = (userId, res) => {
   })
   res.cookie('jwt', token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days,
-    secure: process.env.NODE_ENV === 'development' ? false : true,
-    sameSite: 'strict', // CSRF attacks
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // CSRF attacks
     httpOnly: true, // XSS attacks
   });
   return token;
